5ce279593a1bd48deac81d4ec7e97d4991c12215
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-04-03 19:46:00 -0500

    copy vs view w3schools

---
+++ w3schools/07-copy-vs-view/demo037.py
@@ -0,0 +1,9 @@
+# W3Schools, NumPy Copy vs View
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42
+
+print(arr)
+print(x)
---
+++ w3schools/07-copy-vs-view/demo038.py
@@ -0,0 +1,9 @@
+# W3Schools, NumPy Copy vs View
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+arr[0] = 42
+
+print(arr)
+print(x)
