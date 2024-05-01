fc404537cdfa294198478241b74e038da37946c2
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-05-01 16:55:46 -0500

    random intro w3schools...

---
+++ w3schools/16-random-intro/demo087.py
@@ -0,0 +1,6 @@
+# W3Schools, NumPy Random Intro
+from numpy import random
+
+x = random.rand()
+
+print(x)
---
+++ w3schools/16-random-intro/demo093.py
@@ -0,0 +1,6 @@
+# W3Schools, NumPy Random Intro
+from numpy import random
+
+x = random.choice([3, 5, 7, 9], size=(3, 5))
+
+print(x)
