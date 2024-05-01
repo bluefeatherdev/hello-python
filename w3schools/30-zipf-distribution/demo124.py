55d906f8c7933f30684ed7c7c3d80dc9c92ebb2d
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-05-01 18:35:36 -0500

    zipf distribution w3schools...

---
+++ w3schools/30-zipf-distribution/demo124.py
@@ -0,0 +1,6 @@
+# W3Schools, NumPy Zipf Distribution
+from numpy import random
+
+x = random.zipf(a=2, size=(2, 3))
+
+print(x)
---
+++ w3schools/30-zipf-distribution/demo125.py
@@ -0,0 +1,9 @@
+# W3Schools, NumPy Zipf Distribution
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = random.zipf(a=2, size=1000)
+sns.distplot(x[x < 10], kde=False)
+
+plt.show()
