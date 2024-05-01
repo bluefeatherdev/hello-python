3f06750c4b2efa824f8ea850c7f065e3dd68c913
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-05-01 18:25:30 -0500

    chi square distribution w3schools...

---
+++ w3schools/27-chi-square-distribution/demo118.py
@@ -0,0 +1,6 @@
+# W3Schools, NumPy Chi Square Distribution
+from numpy import random
+
+x = random.chisquare(df=2, size=(2, 3))
+
+print(x)
---
+++ w3schools/27-chi-square-distribution/demo119.py
@@ -0,0 +1,8 @@
+# W3Schools, NumPy Chi Square Distribution
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.chisquare(df=1, size=1000), hist=False)
+
+plt.show()
